2309d1a48ba19434e187b75e8e3c95d5a0c4f5ea
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-04-12 15:44:27 +0530

    Update main.py

--- PyrogramBot/main.py
+++ PyrogramBot/main.py
@@ -70,49 +70,6 @@ async def start_message(bot, message):
     )
 
 
-# Callback
-
-@Client.on_callback_query()
-async def callback(bot, msg):
-    if msg.data == "start":
-        await msg.message.edit(
-            text =f" Hello {msg.from_user.mention} 😁",
-        )
-
-    elif msg.data == "help":
-        await msg.message.edit(
-            text="**My Tools**",
-            reply_markup=InlineKeyboardMarkup([[
-               InlineKeyboardButton("Info", callback_data="ids"),
-               InlineKeyboardButton("soon..", callback_data="start"),
-               InlineKeyboardButton("soon..", callback_data="start")
-               ],[
-               InlineKeyboardButton("🧑‍🦯 Exit", callback_data="delete")
-               ]]
-            )
-        )
-
-    elif msg.data == "ids":
-        await msg.message.edit(
-            text="/id : to get your ID\n/info : to get your information ℹ️\n/ginfo - To get group information 👥\n/dc : to get your datacenter 🗄️",
-            reply_markup=InlineKeyboardMarkup([[
-               InlineKeyboardButton("🧑‍🦯 Exit", callback_data="delete")
-               ]]
-            )
-        )
-
-    elif msg.data == "about":
-        await msg.message.edit(
-            text="╭────[ᴀʙᴏᴜᴛ]────⍟\n├🤖 **Bot Nᴀᴍᴇ**: [Mʏ Craft](https://t.me/Mycraftprojectbot)\n├🧑‍💻 **Mʏ Dᴇᴠ**: [Hʏᴅʀɪx](https://t.me/Hydrix777)\n├📡 **Sᴇʀᴠᴇʀ**: [Railway](https://Railway.app)\n├🔣 **Language**: [Python-3](https://python.org/)\n╰───────────⍟",
-            reply_markup=InlineKeyboardMarkup([[
-               InlineKeyboardButton("🧑‍🦯 Exit", callback_data="delete")
-              ]]
-            )
-        )
-    elif msg.data == "delete":
-        await msg.message.delete()
-
-
 @Client.on_message(filters.private & filters.command("info"))
 async def id(bot, hydrix):
     await hydrix.reply_sticker(
