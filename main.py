6f3622621718bb9ec159d250ee0a1388fb1e6d52
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-04-24 18:53:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Client = Client(
     "PyrogramBot",
     api_hash="7d120384f48b2a86fa2b9e9772a28af6",
     api_id="18891187",
-    bot_token="5259490489:AAFZuTQ2PcQdrYG8_N2eZMfESgUWHsJtDWo",
+    bot_token="5179683696:AAGkEjTDHctlCBoJs7WtmMRP_6E_CEJHMaU",
     plugins=dict(root="PyrogramBot")
 )
 
